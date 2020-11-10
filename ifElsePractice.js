5a6458278e14a842eef409b2520775692fb725ab
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-11-10 13:01:30 +0530

    [Prerna]ADDED: Simulate coin flip

--- ifElsePractice.js
+++ ifElsePractice.js
@@ -43,3 +43,15 @@
         console.log(year + ' is not a leap year');
     }
 }
+
+// 4. Coin flip
+{
+    const HEADS = 0;
+    let coin = Math.floor(Math.random() * 10) % 2;
+    if (coin == HEADS) {
+        console.log("HEADS");
+    }
+    else {
+        console.log("TAILS");
+    }
+}
