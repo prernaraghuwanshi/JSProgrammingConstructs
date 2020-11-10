3178725b01ea995a675a41c74084bc5bf92216b5
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-11-10 11:51:12 +0530

    [Prerna]ADDED: Sum and average of 5 two digit numbers

--- sequencePractice.js
+++ sequencePractice.js
@@ -17,3 +17,12 @@
     let sum = diceNum1 + diceNum2;
     console.log("First Number: " + diceNum1 + " Second Number: " + diceNum2 + " Sum: " + sum);
 }
+
+// 4. Sum and Average of 5 Random 2 digit numbers
+{
+    let sum = 0;
+    for (let index = 0; index < 5; index++) {
+        sum += Math.floor(Math.random() * 90) + 10;
+    }
+    console.log("Sum: " + sum + " Average: " + sum / 5);
+}
