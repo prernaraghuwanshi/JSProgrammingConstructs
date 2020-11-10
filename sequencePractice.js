b8c6e765ee330c8727e37c44f94d70661303ca81
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-11-10 11:46:32 +0530

    [Prerna]ADDED: Add two numbers

--- sequencePractice.js
+++ sequencePractice.js
@@ -9,3 +9,11 @@
     let dice = Math.ceil(Math.random() * 10) % 6;
     console.log("Dice: " + dice);
 }
+
+// 3. Add two random dice numbers
+{
+    let diceNum1 = Math.floor(Math.random() * 6) + 1;
+    let diceNum2 = Math.floor(Math.random() * 6) + 1;
+    let sum = diceNum1 + diceNum2;
+    console.log("First Number: " + diceNum1 + " Second Number: " + diceNum2 + " Sum: " + sum);
+}
