6a1a6d1a7f27235c3aefc4fbcd6a7cd449861e1d
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-11-10 14:24:46 +0530

    [Prerna]ADDED: Print nth harmonic number

--- forLoopPractice.js
+++ forLoopPractice.js
@@ -6,3 +6,14 @@
         console.log(2 ** index);
     }
 }
+
+// 2. Print nth harmonic number
+{
+    const prompt = require('prompt-sync')();
+    let n = parseInt(prompt("Enter the number: "));
+    let harmonicNumber = 0.0;
+    for (let index = 1; index <= n; index++) {
+        harmonicNumber += 1 / index;
+    }
+    console.log("Harmonic number " + harmonicNumber);
+}
