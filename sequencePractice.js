ac24ce00b853a824acf6444c921d782782a5ea26
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-11-10 11:41:00 +0530

    [Prerna]ADDED: Random function to get dice value

--- sequencePractice.js
+++ sequencePractice.js
@@ -1,3 +1,11 @@
 // 1. Random function to get single digit
-let digit = Math.floor(Math.random() * 10);
-console.log(digit);
+{
+    let digit = Math.floor(Math.random() * 10);
+    console.log("Random digit: " + digit);
+}
+
+// 2. Random function to get dice value
+{
+    let dice = Math.ceil(Math.random() * 10) % 6;
+    console.log("Dice: " + dice);
+}
