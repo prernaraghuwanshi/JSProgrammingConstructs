aabaf5f12d40b369c4f37af54002b46b9049bfd1
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-11-10 12:58:51 +0530

    [Prerna]ADDED: Check if given year is a leap year or not

--- ifElsePractice.js
+++ ifElsePractice.js
@@ -32,3 +32,14 @@
     }
     console.log("Day in between? " + result);
 }
+
+// 3. Leap or not
+{
+    const prompt = require('prompt-sync')();
+    let year = prompt('Enter year ');
+    if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+        console.log(year + ' is a leap year');
+    } else {
+        console.log(year + ' is not a leap year');
+    }
+}
